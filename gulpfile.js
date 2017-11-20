e345b2d0c3219ef2561157aa5fec5f506cf66278
ayang10 <ayang014@gmail.com>
2017-11-19 19:02:39 -0500

    missing file

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,19 @@ var gulp = require('gulp'),
 
 
 gulp.task('sass', function () {
+    return gulp.src('./dist/assets/sass/*.scss')
         .pipe(sass())
+        .pipe(gulp.dest('./dist/assets/css'));
 });
 
 gulp.task('watch', function () {
+    gulp.watch('./dist/assets/sass/*.scss', ['sass']);
 });
 
 gulp.task('minify-css', function () {
+    return gulp.src('./dist/assets/css/style.css')
         .pipe(minifyCSS({ keepSpecialComments: 1 }))
+        .pipe(gulp.dest('./dist/assets/minifycss'));
 });
 
 gulp.task('default', ['sass', 'watch']);
